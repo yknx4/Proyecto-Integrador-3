e805b28473e94737997ddc1980f6e28976d50085
Jorge Figueroa <yknx4@outlook.com>
2013-10-25 11:35:10 -0500

    FirstCommit

---
+++ Proyecto Integrador 3/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ResourcePool = Proyecto_Integrador_3.Properties.Resources;
+using System.Drawing;
+using MahApps.Metro.Controls;
+
+
+namespace Proyecto_Integrador_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbTipos.ItemsSource = (from values in Tipos.Usuarios.Values select values).ToList();
+        }
+    }
+}
---
+++ Proyecto Integrador 3/Tipos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Integrador_3
+{
+    static class Tipos
+    {
+        public static readonly Dictionary<int,string> Usuarios;
+        static Tipos()
+        {
+            Usuarios = new Dictionary<int, string>();
+            Usuarios[1] = "General";
+            Usuarios[2] = "Estudiante";
+            Usuarios[3] = "Tercera Edad";
+            Usuarios[4] = "Capacidades Diferentes";
+
+
+        }
+    }
+}
