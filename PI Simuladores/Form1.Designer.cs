776a702425c067d3f5c861ee52e41493917b9ba4
Jorge Figueroa <yknx4@outlook.com>
2013-11-04 14:42:46 -0600

    icon

--- PI Simuladores/Form1.Designer.cs
+++ PI Simuladores/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtTarjeta = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cmbTarjetas = new System.Windows.Forms.ComboBox();
@@ -235,6 +236,7 @@
             this.Controls.Add(this.tb);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.grpServicio);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Simulador Autobuses";
             this.grpServicio.ResumeLayout(false);
